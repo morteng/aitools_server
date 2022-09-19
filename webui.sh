afcfd55b66238107a7704e7de63416e8c59ef755
Seth Robinson <seth@rtsoft.com>
2022-09-19 17:06:27 +0900

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -101,7 +101,7 @@ then
 fi
 
 printf "\n%s\n" "${delimiter}"
-printf "Clone or update stable-diffusion-webui"
+printf "Clone or update aitools_server"
 printf "\n%s\n" "${delimiter}"
 cd "${install_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/, aborting...\e[0m" "${install_dir}"; exit 1; }
 if [[ -d "${clone_dir}" ]]
@@ -109,7 +109,7 @@ then
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
     "${GIT}" pull
 else
-    "${GIT}" clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
+    "${GIT}" clone https://github.com/SethRobinson/aitools_server.git "${clone_dir}"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
