7c1ee4e0042977ce584683b85059f249fb6b9a06
Morten Gulden <morteng@gmail.com>
2023-01-16 14:13:36 +0100

    bugfix

--- webui.py
+++ webui.py
@@ -190,7 +190,7 @@ def webui():
 
         app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-        print("gå hit for å lage klistremerker https://morteng.github.io/klistremerkemaskin?tjener=" + share_url)
+        print("lag klistremerker: https://morteng.github.io/klistremerkemaskin?tjener=" + share_url)
 
         if launch_api:
             create_api(app)
