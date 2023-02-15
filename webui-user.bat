108f5341d2749a3fa461cffdac8cc69ff3aaa000
Morten Gulden <morten.gulden@tekniskmuseum.no>
2023-02-15 21:18:41 +0100

    added authentication

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --port 7860 --xformers
+set COMMANDLINE_ARGS=--listen --port 7860 --xformers --opt-split-attention --medvram
 
 call webui.bat
